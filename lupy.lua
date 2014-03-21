1f495bb55335d124b97b9d629d1b3b00226f96e7
Peer Zeng <uleelx@foxmail.com>
2014-03-21 16:30:23 +0800

    simplify the implementation

--- lupy.lua
+++ lupy.lua
@@ -1,20 +1,19 @@
-local unique = {__index = true, __type__ = true, include = true}
-
 local metamethods = {
   "__add", "__sub", "__mul", "__div", "__mod", "__pow", "__unm", "__concat", "__len",
   "__eq", "__lt", "__le", "__newindex", "__call", "__pairs", "__ipairs", "__gc"
 }
 
 local function new(class, ...)
-  local instance = setmetatable({__class__ = class}, class)
-  if class.__init__ then class.__init__(instance, ...) end
-  return instance
+  local o = setmetatable({__class__ = class}, class)
+  if class.__init__ then class.__init__(o, ...) end
+  return o
 end
 
-local function include(class, m)
-  table.insert(class.__type__, 2, m.__type__[1])
+local function include(m)
+  debug.upvaluejoin(include, 1, debug.getinfo(2, 'f').func, 1)
+  table.insert(_ENV.__type__, 2, m.__type__[1])
   for k, v in pairs(m) do
-    if not unique[k] then class[k] = v end
+    if k ~= "__index" and k ~= "__type__" then _ENV[k] = v end
   end
 end
 
@@ -22,7 +21,7 @@ local function is(self, c)
   return string.match(table.concat(self.__type__, ','), (c or "([^,]+)"))
 end
 
-local function retrieve(self, member_name)
+local function dig(self, member_name)
   local class = self.__class__
   local member = class[member_name]
   if type(member) == "function" then
@@ -34,27 +33,23 @@ local function retrieve(self, member_name)
   end
 end
 
-local Object = {__index = _ENV, __type__ = {"Object"}, is = is}
+local Object = {__index = _ENV, __type__ = {"Object"}, is = is, include = include}
 setmetatable(Object, Object)
 
 local function class(name)
   local env = _ENV
-  local clsname, supername = string.match(name, "([%w_]*)%s*<?%s*([%w_]*)")
-  local newclass = env[clsname]
-  if not newclass then
-    local superclass = env[supername] or Object
-    newclass = {
-      __type__ = {clsname, table.unpack(superclass.__type__)},
-      include = function(m) include(newclass, m) end,
-      __index = retrieve
-    }
-    for _, k in ipairs(metamethods) do newclass[k] = superclass[k] end
-    setmetatable(newclass, {__index = superclass, __call = new})
-    env[clsname] = newclass
+  local name, supername = string.match(name, "([%w_]*)%s*<?%s*([%w_]*)")
+  local self = env[name]
+  if not self then
+    local super = env[supername] or Object
+    self = {__index = dig, __type__ = {name, table.unpack(super.__type__)}}
+    for _, k in ipairs(metamethods) do self[k] = super[k] end
+    setmetatable(self, {__index = super, __call = new})
+    env[name] = self
   end
   debug.upvaluejoin(class, 1, debug.getinfo(2, 'f').func, 1)
-  newclass._end = function() _ENV, _end = env end
-  _ENV = newclass
+  self._end = function() _ENV, _end = env end
+  _ENV = self
 end
 
 return class
